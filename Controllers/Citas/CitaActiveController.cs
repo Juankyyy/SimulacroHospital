73b8ad38c574f53f223beef5cccb606cf80e697a
Juanky <103221572+Juankyyy@users.noreply.github.com>
2024-05-30 18:47:33 -0500

    feat: Cita Active
    Endopint para activar una cita

--- Controllers/Citas/CitaActiveController.cs
+++ Controllers/Citas/CitaActiveController.cs
@@ -15,5 +15,32 @@ namespace SimulacroHospital.AddControllers
         {
             _citaRepository = citaRepository;
         }
+
+        [HttpPut("{id}")]
+        public IActionResult ActiveCita(int id)
+        {
+            var cita = _citaRepository.GetOne(id);
+            
+            if (cita == null)
+            {
+                return BadRequest($"Cita con id {id} no encontrada");
+            }
+
+            try
+            {   
+                if (cita.Estado == "Activo")
+                {
+                    _citaRepository.Active(id);
+                    return Ok($"La cita con id {id} ya está Activa");
+
+                } else {
+                    _citaRepository.Active(id);
+                    return Ok($"La cita con id {id} pasó a estar Activa");
+                }
+            } catch (Exception ex)
+            {
+                return StatusCode(500, $"Error al cambiar el estado de la cita: {ex.Message}");
+            }
+        }
     }
 }
